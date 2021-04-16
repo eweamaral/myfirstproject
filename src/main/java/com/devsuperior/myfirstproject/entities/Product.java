5babf955545a9605786706bdd72247e5c3ace597
eweamaral <eweamaral@gmail.com>
2021-04-16 11:42:42 -0300

    Product memory

--- src/main/java/com/devsuperior/myfirstproject/entities/Product.java
+++ src/main/java/com/devsuperior/myfirstproject/entities/Product.java
@@ -1,29 +1,33 @@
 package com.devsuperior.myfirstproject.entities;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-public class Category implements Serializable {
+public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Long id;
 	private String name;
+	private Double price;
+
+	private Category category;
 	
-	@JsonIgnore
-	private List<Product> products = new ArrayList<>();
-	
-	public Category() {
+	public Product() {
 		
 	}
 
-	public Category(Long id, String name) {
+	/**
+	 * @param id
+	 * @param name
+	 * @param price
+	 * @param category
+	 */
+	public Product(Long id, String name, Double price, Category category) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.price = price;
+		this.category = category;
 	}
 
 	public Long getId() {
@@ -42,13 +46,25 @@ public class Category implements Serializable {
 		this.name = name;
 	}
 
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
-	}
-
-	public List<Product> getProducts() {
-		return products;
 	}
 
 	@Override
@@ -59,9 +75,9 @@ public class Category implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
 		return Objects.equals(id, other.id);
 	}
-
+	
 	
 }
